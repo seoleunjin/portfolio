4b628fd9af99256f36530f134b049326dc2cdd51
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2022-12-21 16:55:48 +0900

    1221

--- vogue/my/js.js
+++ vogue/my/js.js
@@ -9,21 +9,34 @@ $(document).ready(function(){
         $('#gnb li:nth-child(3)').click(function(){
             $('section>div').removeClass('on');
             $('section>div:nth-child(3)').addClass('on');
+            $('nav').removeClass('on');
+            $('section').removeClass('on');
+            $('#btn').fadeIn()
         })
 
 
         $('#gnb li:nth-child(1)').click(function(){
             $('section>div').removeClass('on');
             $('section>div:nth-child(1)').addClass('on');
+            $('nav').removeClass('on');
+            $('section').removeClass('on');
+            $('#btn').fadeIn()
         })
 
 
         $('#gnb li:nth-child(2)').click(function(){
             $('section>div').removeClass('on');
             $('section>div:nth-child(2)').addClass('on');
+            $('nav').removeClass('on');
+            $('section').removeClass('on');
+            $('#btn').fadeIn()
         })
 
 
 
-
+        $('#btn').click(function(){
+            $('nav').addClass('on');
+            $('section').addClass('on');
+            $(this).fadeOut()
+        })
 })
