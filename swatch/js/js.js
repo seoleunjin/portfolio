ce6e2d9939b46ff16b8051fa9e2d6e57d83ac878
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2022-12-29 16:57:48 +0900

    1229

--- swatch/js/js.js
+++ swatch/js/js.js
@@ -59,19 +59,49 @@ $(document).ready(function(){
         $('.p41').css({'bottom':-120-posy/30,'right': 20+posx/30})
         $('.p42').css({'bottom':-180-posy/30,'right': 0+posx/30})
 
-    
-    
-    
-    
-    
-    
     })
     
     
-    
-    
-    
-    
-    
-    
+    // 스크롤바의 위치값 찾기
+    $(window).scroll(function(){
+        let sc = $(this).scrollTop();
+        $('h1').text(sc);
+        // 스크롤은 윈도우로 만들기
+    })
+    // 애니메이트 작성법 .animate({속성명 : 속석값}.지속시간)
+    // 문제3번 li를 클릭했을때, 스크롤탑을 해당 높이로 가게 만들어라.
+    $('#gnb li').click(function(){
+        // 클릭 했을떄 나의 순번 찾기
+        let i = $(this).index();
+        // 순번
+        let ht = $(window).height();
+
+
+        $('html, body').animate({'scrollTop':ht*i},1400,'easeOutBounce');
+        // 와부스크립트언어는 ''를 쓴다.
+        // 윈도우를 사용하면 안된다 (한정적공간이기때문) scrollTop은 css로 못함
+    });
+
+
+    // h1에 마우스가 들어갔을때 나의 위치깂을 찾아라.
+    $('h1').mouseenter(function(){
+        let abc = $(this).offset().top;
+        alert(abc)
+        
+    });
+
+    $('section').mousewheel(function(event,delta){
+
+        if(delta>0){
+            // 이전페이지로 이동
+            let prev = $(this).prev().offset().top;
+            $('html,body').stop().animate({'scrollTop':prev},1400)
+
+        }else if (delta<0){
+            // 다음페이지로 이동
+            let next = $(this).next().offset().top;
+            $('html,body').stop().animate({'scrollTop':next},1400)
+        }
+    })
+
     })
