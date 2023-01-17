5f60a4366d9fe2aef6433449d01299beac45176e
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2023-01-17 09:04:50 +0900

    0116

--- project001/my/js.js
+++ project001/my/js.js
@@ -1,7 +1,10 @@
-// $(document).ready(function(){
-//     $('.bng li').mouseenter(function(){
-//         let i = $(this).index()
-//         $('.nav ul').slideUp()
-//         $('.nav'+(i+1)).slideDown()
-//     })
-// })
+$(document).ready(function(){
+    $('.gnb>ul>li').mouseenter(function(){
+        let i = $(this).index()
+        $('.sub_menu>div').slideUp()
+        $('.menu0'+(i+1)).slideDown()
+    });
+    $('header').mouseleave(function(){
+        $('.sub_menu>div').slideUp()
+    });
+})
