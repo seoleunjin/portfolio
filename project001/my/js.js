5aa16616f91ecd761428d9cb55c8fadcc674b280
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2023-01-27 19:30:35 +0900

    0127

--- project001/my/js.js
+++ project001/my/js.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-
+    // 해더영역 ########################################################
     $('.gnb>ul>li').mouseenter(function(){
         let i = $(this).index()
         $('.sub_menu>div').slideUp()
@@ -9,15 +9,19 @@ $(document).ready(function(){
     $('header').mouseleave(function(){
         $('.sub_menu>div').slideUp()
     });
+
+
+
+    // 슬라이더영역 ########################################################
     // 바를 클릭 했을때 동영상이 전환되라
     $('.bar li').click(function(){
         let i = $(this).index();
-        $('.img_play li').stop().eq(i-1).animate({'opacity':0},800);
-        $('.img_play li').stop().eq(i).animate({'opacity':1},1000);
-        $('.bar li').removeClass('on');
-        $(this).addClass('on');
+        console.log(i)
+        $('.img_play li').stop().animate({'opacity':'0'},800);
+        $('.img_play li').eq(i).stop().animate({'opacity':'1'},800);
+        $('.bar li').removeClass('on')
+        $(this).addClass('on')
     });
-
     // 타이핑효과
     let txt = $('.slider .txt').text();
     let txtN = txt.split('')
@@ -31,12 +35,7 @@ $(document).ready(function(){
     },700)
 
 
-    
-    // top을 누르면 페이지 상단으로 가라
-    $('.top').click(function(){
-        $('html').animate({'scrollTop':0},1000)
-    });
-
+    // 뉴스영역 ########################################################
     // 마우스가 움직이면 따라 다녀라
 
     $('.news').mousemove(function(e){
@@ -47,6 +46,7 @@ $(document).ready(function(){
         // console.log(nht-y) y는 화면처음부터에서 지금까지의 높이 따라서, nth<y
         $('.cursor').css({'opacity':1})
         $('.cursor').css({'left':x-50,top:-(nht-y)-50})
+        // 마우스를 가져다 되면 .sursor p 가 글씨가 바뀌어라 
     });
     // 마우스가 뉴스섹션을 나가면 사라져라
     $('.news').mouseleave(function(){
@@ -63,21 +63,38 @@ $(document).ready(function(){
     });
 
 
-        // 사진이 한장씩 돌아가라
-        let a = 0;
-        setInterval(function(){
-            a++;
-            if(a==4) a=0;
-            $('.intro .activi li').eq(a-1).css({'left':'0'}).animate({'left':'-100%'});
-            $('.intro .activi li').eq(a).css({'left':'100%'}).animate({'left':'0'});
-        },2500);
+    // 소개영역 ########################################################
+    // activi_btn li 을 클릭했을때 activi li가 이동되고 자동으로 돌아라 
+    $('.activi_btn li').click(function(){
+        let i = $(this).index();
+        $('.intro .activi li').eq(i-1).css({'left':'0'}).animate({'left':'-100%'});
+        $('.intro .activi li').eq(i).css({'left':'100%'}).animate({'left':'0'});
+        $('.activi_btn li').removeClass('on')
+        $(this).addClass('on')
+    });
+    let a =0;
+    function timer(){
+        a++;
+        if(a==4) a=0;
+        $('.activi_btn li').eq(a).trigger('click')
+    };
+
+    let silde = setInterval(timer,2000);
         
-        // 사진이 한장씩 돌아갈때 마다 gnb가 하나씩 바뀌어라 
-        // $('.activi_btn li').click(function(){
-        //     let a = $(this).index();
-        //     console.log(a)
-    
-        // });
+        $('.into conBox').mouseenter(function(){
+            clearInterval(silde)
+        });
+
+        $('.into conBox').mouseleave(function(){
+            silde = setInterval(timer,2000);
+        });
+
+
+    // 탑영역
+    // top을 누르면 페이지 상단으로 가라
+    $('.top').click(function(){
+        $('html').animate({'scrollTop':0},1000)
+    });
 
 
 
@@ -97,14 +114,14 @@ if(wd>=1500){
             $('.intro .txtBox').stop().animate({'left':'-1400px'},1000)
             $('.intro .conBox').stop().animate({'right':'-1800px'},1200)
         };
-        if(sc>=3500 && sc<3900){
+        if(sc>=3700 && sc<3900){
             $('.img02').stop().css({'opacity':'1'})
         }
-        if(sc>=3900 && sc<4200){
+        if(sc>=4000 && sc<4200){
             $('.img02').stop().css({'opacity':'0'})
             $('.img03').stop().css({'opacity':'1'})
         }
-        if(sc>=4200 && sc<4800){
+        if(sc>=4200 && sc<4700){
             
             $('.img04').stop().css({'opacity':'1'})
             $('.img03').stop().css({'opacity':'0'})
@@ -127,8 +144,18 @@ if(wd>=1500){
             });
 
         }
+        if(sc>=6200){
+            // 공지 섹션에 들어왔을때 h1이 순서대로 깜빡여라
+            let b = 0;
+            setInterval(function(){
+                b++;
+                $('.ques .conBox h1').eq(b-1).css({'color':'yellow'})
+            },3000)
+        }
     });
 }
+
+
 
 if(wd>=1024 && wd<1499){
     $(window).scroll(function(){
@@ -142,24 +169,61 @@ if(wd>=1024 && wd<1499){
             $('.intro .txtBox').stop().animate({'left':'-1400px'},1000)
             $('.intro .conBox').stop().animate({'right':'-1800px'},1200)
         };
-        if(sc>=2700 && sc<3000){
+        if(sc>=2500 && sc<2700){
             $('.img02').stop().css({'opacity':'1'})
         }
-        if(sc>=3000 && sc<3200){
+        if(sc>=2700 && sc<2800){
             $('.img02').stop().css({'opacity':'0'})
             $('.img03').stop().css({'opacity':'1'})
         }
-        if(sc>=3200 && sc<3400){
+        if(sc>=2800 && sc<3000){
             
             $('.img04').stop().css({'opacity':'1'})
             $('.img03').stop().css({'opacity':'0'})
         }
-        if(sc>=3400 && sc<3600){
+        if(sc>=3000 && sc<3400){
             $('.img04').stop().css({'opacity':'0'})
             $('.img05').stop().css({'opacity':'1'})
         }
+        if(sc>=3400){
+            $('.img05').stop().css({'opacity':'0'})
+        }
     
-        if(sc>=4800){
+        if(sc>=4600){
+            $('.vagen .txtBox h1').eq(0).animate({'opacity':1},800,function(){
+                $('.vagen .imgBox li').eq(0).animate({'opacity':1},600,function(){
+                    $('.vagen .imgBox li').eq(1).animate({'opacity':1},600,function(){
+                        $('.vagen .imgBox li').eq(2).animate({'opacity':1},600,function(){
+                        });
+                    });
+                });
+            });
+        }
+
+        if(sc>=3900){
+            // 공지 섹션에 들어왔을때 h1이 순서대로 깜빡여라
+            let b = 0;
+            setInterval(function(){
+                b++;
+                $('.ques .conBox h1').eq(b-1).css({'color':'yellow'})
+            },3000)
+        }
+    });
+}
+
+if(wd>=768 && wd<1023){
+    $('.toggle').click(function(){
+        $('.gnb').animate({'left':'0'},800)
+    });
+    $('.gnb').mouseleave(function(){
+        $('.gnb').animate({'left':'-100%'},2000)
+    });
+
+    $(window).scroll(function(){
+        
+        let sc = $(this).scrollTop()
+        $('.sc').text(sc)
+        if(sc>=2600){
             $('.vagen .txtBox h1').eq(0).animate({'opacity':1},800,function(){
                 $('.vagen .imgBox li').eq(0).animate({'opacity':1},600,function(){
                     $('.vagen .imgBox li').eq(1).animate({'opacity':1},600,function(){
@@ -170,9 +234,17 @@ if(wd>=1024 && wd<1499){
             });
 
         }
+        if(sc>=2100){
+            // 공지 섹션에 들어왔을때 h1이 순서대로 깜빡여라
+            let b = 0;
+            setInterval(function(){
+                b++;
+                $('.ques .conBox h1').eq(b-1).css({'color':'yellow'})
+            },3000)
+        }
     });
 }
-
+    
 
 
 })
