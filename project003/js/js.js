0e6bb891d78e6243d1aaac419def54bdd471bca6
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2023-02-17 16:48:56 +0900

    0217

--- project003/js/js.js
+++ project003/js/js.js
@@ -1,9 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let ht = $(window).height();
-    $('section').height(ht)
-    $('nav li').click(function(e){
-        e.preveventDefault();
-        let i =$(this).index();
-        $('body.html').stop().animate({screenTop:ht*i},800,'easeInBounce')
-    })
-})
+  
+    $("section").height(ht);
+  
+    $("nav li").click(function (e) {
+      e.preventDefault();
+  
+      let i = $(this).index();
+  
+      //  스타일시트 01번 적용스크립트 $('html,body').stop().animate({scrollTop:ht*i},800,'easeInBounce');
+      $('section').removeClass('on')
+      $('section').eq(i).addClass('on')
+    });
+  
+    let wd = $(window).width();
+    let hwd = $("header").width();
+  
+    $("#wrap").width(wd - hwd);
+  
+    $("#wrap").css({ left: hwd });
+  
+    $(window).resize(function () {
+      let wd = $(window).width();
+      let hwd = $("header").width();
+      let ht = $(window).height();
+  
+      $("section").height(ht);
+  
+      $("#wrap").width(wd - hwd);
+  
+      $("#wrap").css({ left: hwd });
+    });
+  });
+  
