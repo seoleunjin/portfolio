f03eb71f3fcde45213fc3db84af13e9c66822f61
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2023-02-23 18:34:23 +0900

    0223

--- project003/my/js/js04.js
+++ project003/my/js/js04.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+    let ht = $(window).height();
+        $('section').height(ht);
+        $(window).resize(function(){
+            let ht = $(window).height();
+            $('section').height(ht);
+        })
 
 })
