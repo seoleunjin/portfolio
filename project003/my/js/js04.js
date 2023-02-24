f2cb46824430cb4f29cc0036952b7cf9ed3acd05
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2023-02-24 16:48:42 +0900

    0224

--- project003/my/js/js04.js
+++ project003/my/js/js04.js
@@ -1,9 +1,29 @@
-$(document).ready(function(){
-    let ht = $(window).height();
-        $('section').height(ht);
-        $(window).resize(function(){
-            let ht = $(window).height();
-            $('section').height(ht);
-        })
-
-})
+$(document).ready(function () {
+                let i = -1
+                $('h1').click(function () {
+                    i++;
+                    console.log(i)
+                    if(i==0){
+                    $('#wrap>article')
+                        .eq(i)
+                        .css({'transform': 'rotateY(-180deg)'});
+                    $('#wrap>article')
+                        .eq(i+1)
+                        .css({'transform': 'rotateY(0deg)'})
+                    }
+                    if(i==1){
+                        $('#wrap>article')
+                            .eq(i-1)
+                            .css({'transform': 'rotateY(-180deg)'});
+                        $('#wrap>article')
+                            .eq(i)
+                            .css({'transform': 'rotateY(0deg)'})
+                        $('#wrap>article')
+                            .eq(i+1)
+                            .css({'transform': 'rotateY(-180deg)'});
+                        $('#wrap>article')
+                            .eq(i+2)
+                            .css({'transform': 'rotateY(0deg)' , 'z-index':11})
+                    }
+                })
+            })
