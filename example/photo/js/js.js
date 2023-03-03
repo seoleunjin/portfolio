c7cc870ef8a12d5a6d18901ee98cd2fbad070f90
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2023-03-03 17:03:41 +0900

    0303

--- example/photo/js/js.js
+++ example/photo/js/js.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     });
 
 
-    // 화면에서 스크롤바가 움직일떄 상단의 위치값을 찾아라
+    // 화면에서 스크롤바가 움직일때 상단의 위치값을 찾아라
     $(window).scroll(function(){
         let sc = $(window).scrollTop();
         $('h1').text(sc);
--- info/js/js.js
+++ info/js/js.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-    let ht = $(window).height();
-        $('#wrap').height(ht);
-        $(window).resize(function(){
-            let ht = $(window).height();
-            $('#wrap').height(ht);
-        })
-
+    let a = $('article').size();
+    let aWd= $('article').width()
+    let wh = $(window).height()
+    $('section').height(wh)
+    $('section').width((a*(aWd+3)));
+    $('body').height(a*(aWd+3));
+    $('html,body').scrollTop(a*(aWd+3))
+    $(window).resize(function(){
+    let wh = $(window).height()
+    $('section').height(wh)
+    $('section').width((a*(aWd+3)));
+    $('body').height(a*(aWd+3));
+    });
+    $(window).scroll(function(){
+        let sc = $(window).scrollTop();
+        $('section').css({'left':-sc})
+    });
 })
