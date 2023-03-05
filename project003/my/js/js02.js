60223be3f9fe54355f5be5750b909476f0a3b30c
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2023-03-05 21:38:32 +0900

    0305

--- project003/my/js/js02.js
+++ project003/my/js/js02.js
@@ -1,2 +1,13 @@
 $(document).ready(function(){
+    $('section').mousemove(function(e){
+    
+        let x = e.pageX;
+        let y = e.pageY;
+    
+        $('.bg01').css({'top':20-y/-50,'left':-20+x/30})
+        $('.bg02').css({'top':-40-y/100,'left':30+x/-30})
+        $('.bg03').css({'bottom':70-y/-30,'right':10+x/30})
+        $('.bg04').css({'bottom':70-y/30,'left':-100+x/-30})
+
+    })
 })
