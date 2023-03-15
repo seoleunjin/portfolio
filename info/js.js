2f1b71f0d12915416a15c3b34175dcaad447a1c4
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2023-03-16 01:37:22 +0900

    0316

---
+++ info/js.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     let wh = $(window).height();
+//     $('.home').height(wh)
+//     $('html,body').scrollTop(0)
+//     $(window).resize(function(){
+//     $('.home').height(wh)
+//     });
+// });
